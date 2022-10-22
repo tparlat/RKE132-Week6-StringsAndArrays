4479b162f050cb0abb234a4eaa46c44c9d88b970
Triin Parlat <tparlat@tktk.ee>
2022-10-22 22:18:44 +0300

    create week 6 folders

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿string hello = " we are the champions?";
+
+int stingLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 1;
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    if(char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
